a531c6b80528cb2b9b8841063b0a85993ef8401f
raiden <alicewolffernandes@gmail.com>
2026-05-20 23:41:28 -0300

    ex: hello world

---
+++ Exercise001/Exercise001/Ex001.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld;
+
+class Ex001
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Meu nome é Alice");
+
+        Console.WriteLine("Satoru Gojo");
+    }
+}
