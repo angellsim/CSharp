13c509c5c047464fda7827572cde9a092fc586d1
raiden <alicewolffernandes@gmail.com>
2026-05-29 23:16:05 -0300

    add: jogo rpg com enum

---
+++ Aulas/Var/Var/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Var;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var numero = 7;
+
+        Console.WriteLine(numero.GetTypeCode());
+
+        // Int32
+    }
+}
+
